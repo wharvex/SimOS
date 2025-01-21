7517e683c91a1bee889dabdd5f059b6e8952731d
Tim <81175852+wharvex@users.noreply.github.com>
2025-01-21 17:34:31 -0500

    fix gradle config to let Scanner work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "org.example"
@@ -16,4 +17,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.example.Main")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
