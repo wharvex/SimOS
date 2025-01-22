0bd0717df7d2970506b08f44371f7baf71ba20ac
Tim <81175852+wharvex@users.noreply.github.com>
2025-01-21 20:28:46 -0500

    add lanterna dep and assn docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.googlecode.lanterna:lanterna:3.1.3")
 }
 
 tasks.test {
